bfa76f0428721c8185cc88004c959098be122664
Seth Frost <dovekie@gmail.com>
2017-08-07 12:20:40 -0700

    roughing in some routes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # birdverse
 An API for bird taxonomic data
+
+TODO:
+Access raw bird data
+Process raw bird data
+Store bird data in database
+Make bird data available via API
--- birdverse/birdverse.py
+++ birdverse/birdverse.py
@@ -5,8 +5,19 @@ from flask import (Flask, request, session, g, redirect, url_for, abort,
 app = Flask(__name__)
 
 @app.route('/')
-def default():
+@app.route('/<bird>')
+def default(bird=None):
+    if bird:
+        return get_a_bird(bird)
     return 'Welcome!'
+
+@app.route('/populate')
+def populate_bird_data():
+    return 'This route will fetch fresh bird data from outside source'
+
+
+def get_a_bird(bird):
+    return 'getting a bird: {}'.format(bird)
 
 if __name__ == '__main__':
     app.run()
