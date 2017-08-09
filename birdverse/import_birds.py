5e71a38ebb193170e470c4158072f0664bd9fb9a
Seth Frost <dovekie@gmail.com>
2017-08-09 14:45:42 -0700

    starting to process CSV files

--- birdverse/import_birds.py
+++ birdverse/import_birds.py
@@ -8,16 +8,18 @@ local_bird_storage = 'birdverse/temp/raw_bird_data.csv'
 def get_csv(url):
     r = requests.get(url, stream=True)
     print r.status_code
-    with open(local_bird_storage, 'w') as handle:
+    with open(local_bird_storage, 'wb') as handle:
         for block in r.iter_content(1024):
             handle.write(block)
     return local_bird_storage
 
 def process_csv(bird_storage):
-    with open(bird_storage, 'r') as bird_csv:
+    all_birds = []
+    with open(bird_storage, 'rU') as bird_csv:
         bird_reader = csv.reader(bird_csv, dialect=csv.excel_tab)
         for row in bird_reader:
-            print ', '.join(row)
+            all_birds.append(row)
+    return all_birds
 
 
-print process_csv(get_csv(ebird_url))
+print process_csv(get_csv(ebird_url))[1][0].split(',')
