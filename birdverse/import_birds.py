fc1280d41800a21b4647a152730979e6690bbf87
Seth Frost <dovekie@gmail.com>
2017-08-10 10:33:02 -0700

    load birds into database

--- birdverse/import_birds.py
+++ birdverse/import_birds.py
@@ -1,5 +1,13 @@
 import csv
 import requests
+from flask_sqlalchemy import SQLAlchemy
+from birdverse import app
+
+
+db = SQLAlchemy(app)
+
+
+from models import Bird
 
 ebird_url = ('http://help.ebird.org/customer/portal/'
              'kb_article_attachments/91238/original.csv?1471193184')
@@ -14,12 +22,28 @@ def get_csv(url):
     return local_bird_storage
 
 def process_csv(bird_storage):
-    all_birds = []
+    first_line = True
     with open(bird_storage, 'rU') as bird_csv:
         bird_reader = csv.reader(bird_csv, dialect=csv.excel_tab)
         for row in bird_reader:
-            all_birds.append(row)
+            if first_line:
+                headers = row
+                first_line = False
+                continue
+            bird_data = row[0].split(',')
+            print bird_data
+            new_bird = Bird(
+                common_name = bird_data[4],
+                species_name = bird_data[5].split()[1],
+                genus_name = bird_data[5].split()[0],
+                family_name = bird_data[6],
+                order_name = bird_data[7],
+                spuh = bird_data[8]
+                )
+            db.session.add(new_bird)
+            db.session.commit()
     return all_birds
 
 
+print process_csv(get_csv(ebird_url))[0][0].split(',')
 print process_csv(get_csv(ebird_url))[1][0].split(',')
